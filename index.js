1ddb3f202134fa586112dd92de52cfd4b34c397a
Bruno <brunohep86@gmail.com>
2025-05-16 20:29:48 +0200

    feat: adicionado Swipeable nas tasks

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+// index.js
+import React from 'react';
+import { AppRegistry, StyleSheet } from 'react-native';
 import TaskList from './src/screens/TaskList';
-import {name as appName} from './app.json';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => TaskList);
+const App = () => (
+  <GestureHandlerRootView style={styles.container}>
+    <TaskList />
+  </GestureHandlerRootView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+AppRegistry.registerComponent(appName, () => App);
