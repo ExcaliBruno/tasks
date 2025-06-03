68f293f6145c101ad29e5242c51d82e3c7e60ce4
Bruno <brunohep86@gmail.com>
2025-06-03 15:40:58 +0200

    feat: funcao pra registro

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -10,9 +10,13 @@ import {
     Alert
 } from 'react-native'
 
+import axios from 'axios'
+
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
 import AuthInput from '../components/AuthInput'
+
+import {server, showError, showSucess} from '../common'
 
 export default class Auth extends Component{
 
@@ -26,9 +30,25 @@ export default class Auth extends Component{
 
     signinOrSignup = () => {
         if(this.state.stageNew){
-            Alert.alert('Sucesso!', 'Criar conta')
+            this.signup()
         } else {
             Alert.alert('Sucesso!', 'Logar')
+        }
+    }
+
+    signup = async () =>{
+        try{
+            await axios.post(`${server}/signup`, {
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+                confirmPassword: this.state.confirmPassword
+            })
+
+            showSucess('Usuario cadastrado')
+            this.setState({stageNew:false})
+        } catch(e) {
+            showError(e)
         }
     }
 
@@ -114,7 +134,8 @@ const styles = StyleSheet.create({
     input: {
         marginTop: 10,
         backgroundColor: '#FFF',
-        padding: Platform.OS == 'ios' ? 15 : 10
+        padding: Platform.OS == 'ios' ? 15 : 10,
+        color: '#000'
 
 
     },
